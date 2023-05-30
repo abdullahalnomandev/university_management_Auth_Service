d8ae5e4f098b80e9b1bd8d6cfda4f155f031a0f5
abdullahalnomanfb <abdullahalnoman1512@gamil.com>
2023-05-30 18:13:56 +0600

    testing

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,18 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
-import "colorts/lib/string";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
+import 'colorts/lib/string'
 
 const connectToDatabase = async () => {
   try {
-    await mongoose.connect(config.database_url as string);
-    console.log("Database connected successfully.".green);    
-    
-    app.listen(config.port, () => {
-      console.log(`Application listening on ${config.port}`.yellow);
-    });
+    await mongoose.connect(config.database_url as string)
+    console.log('Database connected successfully.'.green)
 
+    app.listen(config.port, () => {
+      console.log(`Application listening on ${config.port}`.yellow)
+    })
   } catch (error) {
-    console.log(error, "Failed to connect." );
+    console.log(error, 'Failed to connect.')
   }
-};
-connectToDatabase();
+}
+connectToDatabase()
