dec22eadcc5a491604defcc22d3f01c64f467852
abdullahalnomanfb <abdullahalnoman1512@gamil.com>
2023-06-02 03:20:18 +0600

    add error

--- src/app/modules/users/users.utils.ts
+++ src/app/modules/users/users.utils.ts
@@ -6,12 +6,11 @@ export const findLastUserId = async () => {
       createdAt: -1,
     })
     .lean()
-
   return lastUser?.id
 }
+
 export const generatedUserId = async () => {
   const currentId = (await findLastUserId()) || (0).toString().padStart(5, '0')
-
   const incrementedId = (Number(currentId) + 1).toString().padStart(5, '0')
   return incrementedId
 }
