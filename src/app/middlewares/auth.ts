6940d43c74aa1a3d1e91138f057e8f951d911259
abdullahalnomanfb <abdullahalnoman1512@gmail.com>
2023-07-20 23:38:38 +0600

    added role

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -5,8 +5,8 @@ import config from '../../config';
 import ApiError from '../../errors/ApiError';
 import { jwtHelper } from '../../helpers/jwtHelpers';
 
-
-const auth =(...requiredRoles: string[]) =>
+const auth =
+  (...requiredRoles: string[]) =>
   async (req: Request, res: Response, next: NextFunction) => {
     try {
       // get authorization  token
@@ -17,15 +17,18 @@ const auth =(...requiredRoles: string[]) =>
 
       // verify token
       let verifiedUser = null;
-      verifiedUser = jwtHelper.verifiedToken( token, config.jwt.secret as Secret);
+      verifiedUser = jwtHelper.verifiedToken(
+        token,
+        config.jwt.secret as Secret
+      );
 
       req.user = verifiedUser;
 
-      console.log(req.user);
-      
-      
-      next();
+      if (requiredRoles.length && !requiredRoles.includes(verifiedUser.role)) {
+        throw new ApiError(httpStatus.UNAUTHORIZED, 'Forbidden');
+      }
 
+      next();
     } catch (error) {
       next(error);
     }
